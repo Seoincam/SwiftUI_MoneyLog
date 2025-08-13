3ede4462e174c1f0576ef875d76c295fca05677e
Seoincam <seoin4715@naver.com>
2025-08-13 12:31:07 +0900

    feat: Transaction 모델 생성

---
+++ MoneyLog/Transaction.swift
@@ -0,0 +1,35 @@
+//
+//  Transaction.swift
+//  MoneyLog
+//
+//  Created by 박서인 on 8/13/25.
+//
+
+import Foundation
+import SwiftData
+
+
+@Model
+final class Transaction {
+    var date: Date
+    var type: TransactionType
+    var amount: Int
+    var note: String?
+    var createdAt: Date
+    
+    init(date: Date,
+         type:TransactionType,
+         amount: Int,
+         note: String? = nil) {
+        self.date = date
+        self.type = type
+        self.amount = amount
+        self.note = note
+        createdAt = Date.now
+    }
+}
+
+enum TransactionType: String, Codable {
+    case income
+    case expense
+}
